86f0af8c9b2811df7be4bc8a4079d67dc430fa09
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2022-02-25 22:58:31 -0300

    fix(path): fix module 0 tasks path

--- mod_0/easy/ex03.py
+++ mod_0/easy/ex03.py
@@ -1,7 +1,7 @@
 #Faça um programa que mostre na tela uma letra de música que você gosta.
 
 import time
-lyrics = open("easy/blame_it_on_the_girls_mika_ex03.txt", "r")
+lyrics = open("mod_0/easy/blame_it_on_the_girls_mika_ex03.txt", "r")
 #print(lyrics.read)
 
 for line in lyrics:
